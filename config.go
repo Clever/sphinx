5f7cb2f73f1c91cae0f67c929ee8bad4077303c7
Mohit Gupta <mohit.ed@gmail.com>
2014-03-27 17:10:19 -0700

    load yaml config, sample config and basic tests

--- config.go
+++ config.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"gopkg.in/v1/yaml"
 	"io/ioutil"
+	"log"
 )
 
 type Configuration struct {
@@ -28,15 +30,46 @@ type Rules struct {
 	Paths   []string
 }
 
-func NewConfiguration(path string) Configuration {
-
-	data, err := ioutil.ReadFile(path)
+func panicWithError(err error, message string) {
 	if err != nil {
-		panic(err)
+		log.Panic(message, err)
 	}
+}
+
+func loadAndValidateConfig(data []byte) (Configuration, error) {
 
 	config := Configuration{}
-	yaml.Unmarshal(data, &config)
+	err := yaml.Unmarshal(data, &config)
+	panicWithError(err, fmt.Sprintf("Failed to parse data in configuration. Aborting"))
+
+	if config.Forward.Scheme == "" {
+		return config, fmt.Errorf("forward.scheme not set")
+	}
+	if config.Forward.Host == "" {
+		return config, fmt.Errorf("forward.host not set")
+	}
+	if len(config.Buckets) < 1 {
+		return config, fmt.Errorf("No buckets definied")
+	}
+
+	for name, bucket := range config.Buckets {
+		if bucket.Interval < 1 {
+			return config, fmt.Errorf("Interval must be set > 1 for bucket: %s", name)
+		}
+		if bucket.Limit < 1 {
+			return config, fmt.Errorf("Limit must be set > 1 for bucket: %s", name)
+		}
+	}
+
+	return config, nil
+}
+
+func NewConfiguration(path string) Configuration {
+	data, err := ioutil.ReadFile(path)
+	panicWithError(err, fmt.Sprintf("Failed to read configuration %s. Aborting", path))
+
+	config, load_err := loadAndValidateConfig(data)
+	panicWithError(load_err, "Failed to validate configuration")
 
 	return config
 }
--- config_test.go
+++ config_test.go
@@ -5,6 +5,8 @@ import (
 )
 
 func TestNewConfiguration(t *testing.T) {
+
+	// test loading example config
 	config := NewConfiguration("./example.yaml")
 
 	if config.Forward.Scheme != "http" {
@@ -29,5 +31,30 @@ func TestNewConfiguration(t *testing.T) {
 		if len(bucket.Matches.Headers) < 1 && len(bucket.Matches.Paths) < 1 {
 			t.Error("One of paths or headers was expected to be set for matches")
 		}
+	}
+
+	// test incorrect config
+	invalid_config := []byte(`
+forward:
+  host: proxy.example.com
+`)
+	config, err := loadAndValidateConfig(invalid_config)
+	if err == nil {
+		t.Error("invalid config did not return error")
+	}
+
+	invalid_config = []byte(`
+forward:
+  scheme: http
+  host: proxy.example.com
+
+buckets:
+  bearer/events:
+    keys:
+      - 'header:authentication'
+`)
+	config, err = loadAndValidateConfig(invalid_config)
+	if err == nil {
+		t.Error("invalid config did not return error")
 	}
 }
