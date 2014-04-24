6f088ac70974ffdbc512f3b3cc9f320d3ba907ab
Alex Zylman <alex.zylman@getclever.com>
2014-04-24 16:24:16 -0700

    handlers: http: add HTTPRateLogger

--- handlers/http.go
+++ handlers/http.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Clever/leakybucket"
 	"github.com/Clever/sphinx"
 	"github.com/Clever/sphinx/common"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -24,7 +25,7 @@ type HTTPRateLimiter struct {
 func (hrl HTTPRateLimiter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	matches, err := hrl.ratelimiter.Add(parseRequest(r))
 	if err != nil && err != leakybucket.ErrorFull {
-		// TODO: Send to sentry.
+		// TODO: Log to stderr
 		w.WriteHeader(500)
 		return
 	}
@@ -32,6 +33,22 @@ func (hrl HTTPRateLimiter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if err == leakybucket.ErrorFull {
 		w.WriteHeader(429)
 		return
+	}
+	hrl.proxy.ServeHTTP(w, r)
+}
+
+type HTTPRateLogger HTTPRateLimiter
+
+func (hrl HTTPRateLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	matches, err := hrl.ratelimiter.Add(parseRequest(r))
+	if err != nil && err != leakybucket.ErrorFull {
+		log.Printf("ERROR")
+		hrl.proxy.ServeHTTP(w, r)
+		return
+	}
+	log.Printf("RATE LIMIT HEADERS: %#v", getRateLimitHeaders(matches))
+	if err == leakybucket.ErrorFull {
+		log.Printf("FULL")
 	}
 	hrl.proxy.ServeHTTP(w, r)
 }
@@ -44,15 +61,43 @@ func int64ToString(num int64) string {
 	return strconv.Itoa(int(num))
 }
 
-func addRateLimitHeaders(w http.ResponseWriter, statuses []sphinx.Status) {
+func initHeaders() map[string][]string {
+	headers := map[string][]string{}
+	for _, header := range []string{"Limit", "Reset", "Remaining", "Bucket"} {
+		headerName := "X-Rate-Limit-" + header
+		if headers[headerName] == nil {
+			headers[headerName] = []string{}
+		}
+	}
+	return headers
+}
+
+func getRateLimitHeaders(statuses []sphinx.Status) map[string][]string {
+	if len(statuses) == 0 {
+		return map[string][]string{}
+	}
+	headers := initHeaders()
 	for _, status := range statuses {
-		w.Header().Add("X-Rate-Limit-Limit", uintToString(status.Capacity))
-		w.Header().Add("X-Rate-Limit-Reset", int64ToString(status.Reset.Unix()))
-		w.Header().Add("X-Rate-Limit-Remaining", uintToString(status.Remaining))
-		w.Header().Add("X-Rate-Limit-Bucket", status.Name)
+		headers["X-Rate-Limit-Limit"] = append(headers["X-Rate-Limit-Limit"], uintToString(status.Capacity))
+		headers["X-Rate-Limit-Reset"] = append(headers["X-Rate-Limit-Reset"], int64ToString(status.Reset.Unix()))
+		headers["X-Rate-Limit-Remaining"] = append(headers["X-Rate-Limit-Remaining"], uintToString(status.Remaining))
+		headers["X-Rate-Limit-Bucket"] = append(headers["X-Rate-Limit-Bucket"], status.Name)
+	}
+	return headers
+}
+
+func addRateLimitHeaders(w http.ResponseWriter, statuses []sphinx.Status) {
+	for header, values := range getRateLimitHeaders(statuses) {
+		for _, value := range values {
+			w.Header().Add(header, value)
+		}
 	}
 }
 
 func NewHTTPLimiter(ratelimiter sphinx.RateLimiter, proxy http.Handler) HTTPRateLimiter {
-	return HTTPRateLimiter{ratelimter: ratelimiter, proxy: proxy}
+	return HTTPRateLimiter{ratelimiter: ratelimiter, proxy: proxy}
+}
+
+func NewHTTPLogger(ratelimiter sphinx.RateLimiter, proxy http.Handler) HTTPRateLogger {
+	return HTTPRateLogger{ratelimiter: ratelimiter, proxy: proxy}
 }
