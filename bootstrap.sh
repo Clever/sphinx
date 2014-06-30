521c92bc0edc71d07b74489879dbb2b24230b7b5
Alex Zylman <alex.zylman@getclever.com>
2014-06-30 10:38:40 -0700

    add a vagrant config for sphinx development

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y make git mercurial
+wget https://godeb.s3.amazonaws.com/godeb-amd64.tar.gz -O /tmp/godeb-amd64.tar.gz
+tar xvzf /tmp/godeb-amd64.tar.gz
+mv godeb /usr/local/bin
+godeb install 1.2
+mkdir -p /home/vagrant/.go
+chown vagrant:vagrant /home/vagrant/.go
+echo "export GOPATH=~/.go" >> /home/vagrant/.bashrc
+# Git repo is mounted to /vagrant, start there
+echo "cd /vagrant" >> /home/vagrant/.bashrc
